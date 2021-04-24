d63702efc8f05f8aa0cf5d4458c9986084ad1694
vjta088x <growtopian52@gmail.com>
2021-04-24 07:27:40 +0200

    Inventory class

---
+++ core/src/ldjam48/game/gui/Inventory.java
@@ -0,0 +1,11 @@
+package ldjam48.game.gui;
+
+import com.badlogic.gdx.math.Vector2;
+import ldjam48.game.node.Node;
+
+public class Inventory extends Node {
+
+    public Inventory() {
+        super("Inventory");
+    }
+}
