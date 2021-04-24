f5ba057802f2d587e92a1c5a8ca9157313a074b3
Adam Heczko <adam.heczko@gmail.com>
2021-04-24 07:17:11 +0200

    Added tilemap, blocks

--- core/src/ldjam48/game/blocks/BlockMeta.java
+++ core/src/ldjam48/game/blocks/BlockMeta.java
@@ -1,14 +1,22 @@
 package ldjam48.game.blocks;
 
+import com.badlogic.gdx.graphics.Texture;
+
 import java.util.HashMap;
 
 public class BlockMeta {
     private boolean isCollidable;
     private boolean isBackground;
+    private Texture texture;
     public HashMap<Object, Object> customMetaValues = new HashMap<>();
 
-    public BlockMeta(boolean isCollidable, boolean isBackground) {
+    public BlockMeta(boolean isCollidable, boolean isBackground, Texture texture) {
         this.isCollidable = isCollidable;
         this.isBackground = isBackground;
+        this.texture = texture;
+    }
+
+    public Texture getTexture() {
+        return texture;
     }
 }
